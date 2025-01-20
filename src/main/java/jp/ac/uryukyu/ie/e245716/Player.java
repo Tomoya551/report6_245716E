180ec87a60e901cc36dd57831dde24909f44ae74
Tomoya YAMAKAWA <e245716@ie.u-ryukyu.ac.jp>
2025-01-20 23:00:37 +0900

    5th commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e245716/Player.java
@@ -0,0 +1,22 @@
+package main.java.jp.ac.uryukyu.ie.e245716;
+
+import java.util.Scanner;
+
+class Player {
+    private Hand hand;
+
+    public Player() {
+        hand = new Hand();
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public boolean decideToDraw() {
+        System.out.println("カードを引きますか？ Yes:y or No:n");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        return "y".equalsIgnoreCase(input);
+    }
+}
